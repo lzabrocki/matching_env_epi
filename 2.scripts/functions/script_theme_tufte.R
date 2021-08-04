ca7dafcc5e8f9d1996f4faea89e48605bf0b0d47
lzabrocki <leo.zabrocki@gmail.com>
2021-08-04 14:56:54 +0200

    Moving files to GitHub

---
+++ 2.scripts/functions/script_theme_tufte.R
@@ -0,0 +1,85 @@
+theme_tufte <- function() {
+  # assign font family up front
+  font <- "Roboto Condensed"
+  
+  #replace elements we want to change
+  theme_bw() %+replace%
+    
+    theme(
+      # panel borders
+      panel.border = element_blank(),
+      #grid elements
+      panel.grid.major.y = element_blank(),
+      panel.grid.minor.y = element_blank(),
+      panel.grid.minor.x = element_blank(),
+      panel.grid.major.x = element_blank(),
+      # titles parameters
+      plot.title = element_text(
+        hjust = 0,
+        family = font,
+        size = 18,
+        face = "bold",
+        margin = ggplot2::margin(b = 0.2, unit = "cm")
+      ),
+      plot.subtitle = element_text(
+        hjust = 0,
+        family = font,
+        size = 12
+      ),
+      # axis titles parameters
+      axis.title.x = ggplot2::element_text(
+        family = font,
+        hjust = 1,
+        size = 12,
+        margin = margin(t = 0.2, unit = "cm")
+      ),
+      axis.title.y = ggplot2::element_text(
+        family = font,
+        hjust = 1,
+        size = 12,
+        angle = 90,
+        margin = margin(r = 0.2, unit = "cm")
+      ),
+      # axis texts
+      axis.text.x = element_text(
+        family = font,
+        color = "black",
+        size = 8,
+        margin = margin(t = 0.2, unit = "cm")
+      ),
+      axis.text.y = element_text(
+        family = font,
+        color = "black",
+        size = 8,
+        margin = margin(r = 0.2, unit = "cm")
+      ),
+      # axis ticks
+      axis.ticks.x = element_line(size = 0.1, color = "black"),
+      axis.ticks.y = element_line(size = 0.1, color = "black"),
+      axis.ticks.length = unit(0.15, "cm"),
+      # facet texts
+      strip.background = element_blank(),
+      strip.text.x = element_text(
+        family = font,
+        hjust = 0,
+        size = 16
+      ),
+      strip.text.y = element_text(
+        family = font,
+        hjust = 0,
+        size = 16
+      ),
+      # legend parameters
+      legend.position = "top",
+      legend.justification = "left",
+      legend.direction = "horizontal",
+      legend.title = element_text(
+        family = font,
+        size = 12,
+        face = "bold"
+      ),
+      legend.text = element_text(family = font, size = 12)
+      #plot.margin = margin(0.25, 0.25, 0.25, 0.25, "cm")
+    )
+}
+
